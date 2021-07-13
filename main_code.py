5001510a1ac3064a88fc21a8f0a9d94b47895c6c
chingchenn <87369245+chingchenn@users.noreply.github.com>
2021-07-13 18:59:12 +0800

    Add files via upload

---
+++ main_code.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 20:42:11 2020
+
+@author: jiching
+"""
+    ############## python 下載波形兼畫cross section的程式  ##############
+
+import pandas as pd
+from toRTZ import enztortz
+from waveform_plot_all_bob import WaveformPlotAll
+from download_waveform_bob import download_waveform
+from waveform_plot_indiv_bob import WaveformPlotIndiv
+
+     ######### Obervation Setup ##########
+dleft, dright = 0, 30*60                # waveform cut length (left and right relative to origin time)
+waveform_type = 'DISP'
+model_name = "ak135"
+freqmin = 0.01
+freqmax = 0.1
+minlat = -75
+maxlat = 75
+minlon = -179
+maxlon = 179
+
+    ############## DO WHAT ###############
+download_remove_ins = 0                 # Download data and remove instrument response
+download_data = 0                       # Download data 
+remove_ins = 0                          # remove instrument response
+                                        
+waveform_plot_individual = 1            # Plot individual waveforms 
+waveform_plot_all_in_distance = 1       # Plot the waveform of all stations (arrange in distance)
+ENZtoRTZ = 1
+############## read catalog ##############
+i = 15
+event_info_df = pd.read_csv("event_catalog.csv")
+event_name = event_info_df.loc[i,'event_name']
+time = event_info_df.loc[i,'origin_time']
+# latitude and longitude
+cevent = [ event_info_df.loc[i,'latitude'], event_info_df.loc[i,'longitude'] ]
+
+# files define
+download_dir = event_name + "_projects/"
+event_catalog = "event_catalog.csv"
+################ download the data & plot stations##############
+if download_remove_ins:
+    download_waveform(event_name, time, cevent, dleft, dright, waveform_type,
+                      minlat,minlon,maxlat,maxlon,
+                      download_data = download_data,
+                      remove_ins = remove_ins,
+                      download_dir = download_dir)
+if waveform_plot_all_in_distance:
+    WaveformPlotAll(i,'BHZ',event_name,waveform_type,dleft=dleft, download_dir=download_dir,event_catalog=event_catalog,model_name=model_name, freqmin=freqmin, freqmax=freqmax)
+################ plot waveform for individual stations ##############
+if waveform_plot_individual:
+    WaveformPlotIndiv(i,event_name,waveform_type,dleft=dleft, download_dir=download_dir,event_catalog=event_catalog,model_name=model_name, freqmin=freqmin, freqmax=freqmax)
+## ENZ to RTZ
+if ENZtoRTZ:
+    enztortz(i,event_name,waveform_type,dleft=dleft, download_dir=download_dir,event_catalog=event_catalog)    
